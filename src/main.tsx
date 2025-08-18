48630a83bd3f0287aa5515b74e75eecdb251b9e6
Borjanov Dmitry <33694303+toyo12312@users.noreply.github.com>
2025-08-18 08:19:31 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async'; // 1. Імпортуємо провайдер
+import { HelmetProvider } from 'react-helmet-async'; 
 import App from './App';
 import './index.css';
 import './i18n/i18n';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* 2. Огортаємо додаток у провайдер */}
     <HelmetProvider>
       <BrowserRouter>
         <App />
